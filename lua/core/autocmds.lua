e3cee7ceff70fd1cb3407933ae98e2b822519317
Luke <luke.mitchelson@outlook.com>
2025-11-15 20:56:35 +1100

    removed fzf-lua for now

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -14,8 +14,3 @@ vim.api.nvim_create_autocmd({ "FileType", "TextChanged", "InsertLeave" }, {
 	callback = vim.schedule_wrap(ts_diagnostic_func),
 })
 
-vim.api.nvim_create_user_command("FzfLuaMyProjectFiles", function()
-	require("fzf-lua").files({
-		--search_dirs =  {"~./projects"}
-	})
-end, {})
