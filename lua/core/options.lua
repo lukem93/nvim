c13440b65a64959a9448e3df1226c02c4c36978e
Luke <luke.mitchelson@outlook.com>
2025-11-23 01:51:16 +1100

    refactored options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
+vim.cmd("set shortmess+=I")
+
 vim.g.netrw_banner = 0
 vim.g.netrw_browse_split = 3
 
@@ -16,10 +18,9 @@ vim.opt.cmdheight = 0
 vim.opt.showtabline = 1
 vim.o.termguicolors = true
 
-vim.o.completeopt = "menuone,noinsert"
-
--- max height of completion menu
+-- autocomplete menu
 vim.o.pumheight = 12
+vim.o.completeopt = "menuone,noinsert"
 
 -- change dir to current file
 vim.o.autochdir = true  
@@ -30,11 +31,8 @@ vim.opt.autoindent = false
 vim.opt.smartindent = false
 vim.opt.cindent = false
 
-vim.cmd("set shortmess+=I")
-
--- set colour scheme
---vim.cmd([[colorscheme retrobox]])
 vim.cmd([[colorscheme slate]])
+--vim.cmd([[colorscheme retrobox]])
 --vim.cmd([[colorscheme unokai]])
 --vim.cmd([[colorscheme sorbet]])
 
